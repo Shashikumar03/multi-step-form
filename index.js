3a79fed927e6c9409553123865a3a1c3fb960a1b
ShashiKumar03 <shashikumarkushwaha3@gmail.com>
2023-12-28 22:39:05 +0530

    toggle button

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function nextPage(e) {
   nextButton.id = buttonId.toString();
   if (buttonId == 2) {
     const mainFormDiv = document.getElementById("main-form-div");
-    mainFormDiv.style.paddingBottom = "1.2rem";
+    mainFormDiv.style.paddingBottom = "1.4rem";
     const infoDiv = document.getElementById("info");
     infoDiv.children[0].textContent = "Select your plan";
     infoDiv.children[1].textContent =
